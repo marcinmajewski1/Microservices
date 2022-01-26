261a07f63748a16ca2f81a445bc66081a8754aec
marcinma <marcin.majewski@atena.pl>
2022-01-26 23:05:29 +0100

    Add ClientsServiceProject

---
+++ Backend/ClientsService/Src/Clients/Controllers/ClientsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clients.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Pierwszy klient", "Drugi klient", "Trzeci klient" };
+        }
+    }
+}
